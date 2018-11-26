32f4bc5cffba92f2b14a56e64065dec7e9dd46a0
Evander <evanderhao930@gmail.com>
2018-11-26 12:41:55 +0800

    some new features

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 #encoding: utf-8
-#author: peiwen
+#author: Peiwen
 import time
 import itchat
 #from itchat.content import *
 import re
 from configparser import ConfigParser
 
-d = {
+math_dict = {
     '加':'+',
     '减':'-',
     '乘':'*',
@@ -19,48 +19,79 @@ d = {
 config = ConfigParser()
 config.read('ID.config', encoding='UTF-8') 
 
-d_id = [config['ID']['Size'],config['ID']['Id'],config['ID']['Phone']]
+d_id = [config['ID']['Size'], config['ID']['Id'], config['ID']['Phone']]
 
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
 
 def Trans(matched):
     value = matched.group('value')
-    if value in d:
-        return d[value]
+    if value in math_dict:
+        return math_dict[value]
     else:
         return value
 
-def InputS1(s1):
-    s = re.search(u"问题：[\S]+=？",s1).group()[3:-2]
+def Math(text):
+    s = re.search(u"问题：[\S]+=？",text).group()[3:-2]
     m =re.sub(u"(?P<value>[\u4e00-\u9fa5]+)",Trans,s)
     return str(int(eval(m)))
 
-def InputS2(s2):
-    pattern = re.compile(r"([a-zA-Z0-9]+)，([a-zA-Z0-9]+)，([a-zA-Z0-9]+)")
-    s = re.search(pattern,s2).group().split('，')
+
+def Example(text):
+    pattern = re.compile(u"例如：\S+\n")
+    s = re.search(pattern, text).group().split('，')
+    s[0] = s[0][3:]
+    s[-1] = s[-1][:-1]
     ans = ''
     for i in s:
-        if len(i) == 1:
+        if len(i) == 1 and is_number(i):
             ans += d_id[0]+'，'
-        elif len(i) == 18:
+        elif len(i) == 18 and is_number(i):
             ans += d_id[1]+'，'
-        elif len(i) == 11:
+        elif len(i) == 11 and is_number(i):
             ans += d_id[2]+'，'
+        else:
+            ans += i +'，'
     ans = ans[:-1]
     return ans
+
+def YEEZY_repeat(text, city):
+    pattern = re.search(u"该城市的", text).group()
+    return 'YEEZY'+city
 
 @itchat.msg_register(itchat.content.TEXT, isMpChat=True)
 def text_reply(msg):
     flag = 0
     try:
-        ans = InputS1(msg.text)
+        ans = Math(msg.text)
         flag = 1
     except:
         pass
     try:
-        ans = InputS2(msg.text)
+        ans = Example(msg.text)
         flag =1
     except:
         pass
+    try:
+        ans = YEEZY_repeat(msg.text, '上海')
+        flag =1
+    except:
+        pass
+
     if flag == 1:
         return ans
     
@@ -68,15 +99,21 @@ def text_reply(msg):
 def reply(msg):
     flag = 0
     try:
-        ans = InputS1(msg.text)
+        ans = Math(msg.text)
         flag = 1
     except:
         pass
     try:
-        ans = InputS2(msg.text)
+        ans = Example(msg.text)
         flag =1
     except:
         pass
+    try:
+        ans = YEEZY_repeat(msg.text, '上海')
+        flag =1
+    except:
+        pass
+
     if flag == 1:
         return ans
     
